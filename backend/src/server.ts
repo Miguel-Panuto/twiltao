112ef77b47f152c9d6a22538392b73971fc9e0e5
Miguel Panuto <miguel.panuto@live.com>
2020-06-01 23:02:16 -0300

    server

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const PORT = process.env.PORT || 3333;
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
